b03e77669d5c91041e0611789e0981eea99ba424
Manuel Vézina <manuel.vezina@gmail.com>
2023-07-04 07:28:40 -0400

    New feature + new update checker

--- src/main/java/me/mvez73/furnaceenhanced/FurnaceEnhanced.java
+++ src/main/java/me/mvez73/furnaceenhanced/FurnaceEnhanced.java
@@ -1,5 +1,8 @@
 package me.mvez73.furnaceenhanced;
 
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
+import com.jeff_media.updatechecker.UserAgentBuilder;
 import me.mvez73.furnaceenhanced.events.FurnacesListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,13 +13,23 @@ public final class FurnaceEnhanced extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
-        this.getServer().getPluginManager().registerEvents(new FurnacesListener(this), this);
+        getServer().getPluginManager().registerEvents(new FurnacesListener(), this);
         int pluginId = 15824; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
+
+        new UpdateChecker(this, UpdateCheckSource.SPIGOT, "103489") // You can also use Spiget instead of Spigot - Spiget's API is usually much faster up to date.
+                .setDownloadLink("https://www.spigotmc.org/resources/furnaceenhanced-1-17-1-1-20.103489/") // You can either use a custom URL or the Spigot Resource ID
+                .setDonationLink("https://paypal.me/mvez73")
+                .setNotifyOpsOnJoin(true) // Notify OPs on Join when a new version is found (default)
+                .setUserAgent(new UserAgentBuilder().addPluginNameAndVersion()).checkEveryXHours(12) // Check every 12 hours
+                .checkNow(); // And check right now
+
+        getLogger().info("Successfully enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Disabled");
     }
 }
--- src/main/java/me/mvez73/furnaceenhanced/events/FurnacesListener.java
+++ src/main/java/me/mvez73/furnaceenhanced/events/FurnacesListener.java
@@ -1,39 +1,40 @@
 package me.mvez73.furnaceenhanced.events;
 
-import me.mvez73.furnaceenhanced.FurnaceEnhanced;
 import org.bukkit.block.Block;
+import org.bukkit.block.BrewingStand;
 import org.bukkit.block.Furnace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BrewingStartEvent;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
 import org.bukkit.event.inventory.FurnaceStartSmeltEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 
 public class FurnacesListener implements Listener {
-    public FurnacesListener(final FurnaceEnhanced plugin) {
-    }
+
     public Player player;
     int cookTimeReduction;
     int burnTimeReduction;
+    int brewTimeReduction;
 
     @EventHandler
     public void onPlayerInteract (PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
-        if (block != null && block.getState() instanceof Furnace) {
+        if (block != null && (block.getState() instanceof Furnace || block.getState() instanceof BrewingStand)) {
             player = event.getPlayer();
         }
     }
 
     @EventHandler
-    public void onFurnaceBurnEvent (FurnaceBurnEvent event) {
+    public void onFurnaceBurn (FurnaceBurnEvent event) {
         Block block = event.getBlock();
         if (block.getState() instanceof Furnace) {
             int burnTime = event.getBurnTime();
             int[] timeArray = new int[]{ 90,80,70,60,50,40,30,20,10 };
             for (int percent : timeArray) {
-                if (player.hasPermission("furnaceenhanced." + percent)) {
+                if (player != null && player.hasPermission("furnaceenhanced." + percent)) {
                     burnTimeReduction = burnTime * percent / 100;
                     event.setBurnTime((short) (burnTime - burnTimeReduction));
                 }
@@ -46,10 +47,26 @@ public class FurnacesListener implements Listener {
         int cookTime = event.getTotalCookTime();
         int[] timeArray = new int[]{ 90,80,70,60,50,40,30,20,10 };
         for (int percent : timeArray) {
-            if (player.hasPermission("furnaceenhanced." + percent)) {
+            if (player != null && player.hasPermission("furnaceenhanced." + percent)) {
                 cookTimeReduction = cookTime*percent/100;
                 event.setTotalCookTime(cookTime - cookTimeReduction);
                 return;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBrewingStart (BrewingStartEvent event) {
+        Block block = event.getBlock();
+        if (block.getState() instanceof BrewingStand) {
+            int brewTime = event.getTotalBrewTime();
+            int[] timeArray = new int[]{ 90,80,70,60,50,40,30,20,10 };
+            for (int percent : timeArray) {
+                if (player != null && player.hasPermission("furnaceenhanced." + percent)) {
+                    brewTimeReduction = brewTime*percent/100;
+                    event.setTotalBrewTime(brewTime - brewTimeReduction);
+                    return;
+                }
             }
         }
     }
