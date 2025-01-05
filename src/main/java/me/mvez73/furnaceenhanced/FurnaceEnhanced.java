c8bbf852b4a1cbcf9f048d367cf7ecbbb31cfffe
Manuel Vézina <manuel.vezina@gmail.com>
2025-01-05 17:42:17 -0500

    Fixed missing code and added config.yml Added missing code for update checker Added config.yml to enable/disable update checker

--- src/main/java/me/mvez73/furnaceenhanced/FurnaceEnhanced.java
+++ src/main/java/me/mvez73/furnaceenhanced/FurnaceEnhanced.java
@@ -1,5 +1,8 @@
 package me.mvez73.furnaceenhanced;
 
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
+import com.jeff_media.updatechecker.UserAgentBuilder;
 import me.mvez73.furnaceenhanced.events.FurnacesListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +14,19 @@ public final class FurnaceEnhanced extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new FurnacesListener(), this);
         int pluginId = 15824; // <-- Replace with the id of your plugin!
         new Metrics(this, pluginId);
+
+        int checkEveryXHours = getConfig().getInt("update-checker-checkeveryxhours", 12);
+
+        if (getConfig().getBoolean("update-checker-enabled", true)) {
+            new UpdateChecker(this, UpdateCheckSource.SPIGOT, "103489") // You can also use Spiget instead of Spigot - Spiget's API is usually much faster up to date.
+                    .setDownloadLink("https://www.spigotmc.org/resources/furnaceenhanced-1-17-1-1-20.103489/") // You can either use a custom URL or the Spigot Resource ID
+                    .setDonationLink("https://paypal.me/mvez73")
+                    .setNotifyOpsOnJoin(true) // Notify OPs on Join when a new version is found (default)
+                    .setUserAgent(new UserAgentBuilder().addPluginNameAndVersion()).checkEveryXHours(getConfig().getInt("update-checker-checkeveryxhours", 12)) // Check every 12 hours
+                    .checkNow(); // And check right now
+            getLogger().info("Update checker is enabled and will check every " + checkEveryXHours + (checkEveryXHours == 1 ? " hour" :" hours"));
+        }
+
     }
 
     @Override
